ac234da0971a2d761003be9436abb109f4f4ba9a
Nigel Maddocks <nigel.maddocks@nhs.net>
2019-01-16 13:05:43 +0000

    Fix for occasional Thymeleaf fallover when Ajax method returns void

--- purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/controllers/search/SolutionByCapabilityController.java
+++ purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/controllers/search/SolutionByCapabilityController.java
@@ -8,10 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.swagger.client.model.Solutions;
@@ -101,6 +103,7 @@ public class SolutionByCapabilityController {
      * To be used as an Ajax method from the solutionByCapability page to update the procurement record with the current capabilities
      *  */
     @GetMapping(value = ENDPOINT_UPDATE_PROCUREMENT_WITH_CAPABILITIES + "{procurementId}/{csvCapabilities}")
+    @ResponseStatus(value = HttpStatus.OK)
     public void updateProcurementWithCapabilities (
     		@PathVariable("procurementId") Long procurementId,
     		@PathVariable("csvCapabilities") String csvCapabilities,
