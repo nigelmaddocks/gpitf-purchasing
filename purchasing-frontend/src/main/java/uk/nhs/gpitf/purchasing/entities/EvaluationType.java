8033510a03eb094e23015f9a829062ad0e5cb063
James Moorcroft <james.moorcroft@nhs.net>
2019-04-12 15:38:18 +0100

    EvaluationType enum stuff

--- purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/entities/EvaluationType.java
+++ purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/entities/EvaluationType.java
@@ -4,9 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,13 +13,13 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 public class EvaluationType {
-	
-	public static final long PRICE_ONLY = 1;
-	public static final long PRICE_AND_QUALITY = 2;
+
+//	public static final long PRICE_ONLY = 1;
+//	public static final long PRICE_AND_QUALITY = 2;
 
 	@Id
 	private long id;
 	@Size(max = 255)
 	private String name;
-	
+
 }
---
+++ purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/entities/EvaluationTypeConverter.java
@@ -0,0 +1,19 @@
+package uk.nhs.gpitf.purchasing.entities;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class EvaluationTypeConverter implements AttributeConverter<EvaluationTypeEnum, Long> {
+
+  @Override
+  public Long convertToDatabaseColumn(EvaluationTypeEnum attribute) {
+    return attribute.getId();
+  }
+
+  @Override
+  public EvaluationTypeEnum convertToEntityAttribute(Long dbData) {
+    return EvaluationTypeEnum.getById(dbData);
+  }
+
+}
