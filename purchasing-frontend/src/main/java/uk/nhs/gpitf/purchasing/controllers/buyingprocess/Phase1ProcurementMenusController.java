6db3e08a1612caee522652eeac3ea579bde98a1c
Nigel Maddocks <nigel.maddocks@nhs.net>
2019-05-02 12:38:22 +0100

    Single Site Continuity choice screen

--- purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/controllers/buyingprocess/Phase1ProcurementMenusController.java
+++ purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/controllers/buyingprocess/Phase1ProcurementMenusController.java
@@ -18,11 +18,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uk.nhs.gpitf.purchasing.entities.EvaluationTypeEnum;
 import uk.nhs.gpitf.purchasing.entities.Procurement;
+import uk.nhs.gpitf.purchasing.entities.SingleSiteContinuityEnum;
 import uk.nhs.gpitf.purchasing.entities.SolutionSetEnum;
 import uk.nhs.gpitf.purchasing.exception.ProcurementNotFoundException;
 import uk.nhs.gpitf.purchasing.models.SearchSolutionByCapabilityModel;
 import uk.nhs.gpitf.purchasing.models.TmpBuyingStartModel;
 import uk.nhs.gpitf.purchasing.models.view.buyingprocess.ChoosePriceDeclarationView;
+import uk.nhs.gpitf.purchasing.models.view.buyingprocess.ChooseSingleSiteContinuityView;
 import uk.nhs.gpitf.purchasing.models.view.buyingprocess.ChooseSolutionSetView;
 import uk.nhs.gpitf.purchasing.services.ProcurementService;
 import uk.nhs.gpitf.purchasing.utils.SecurityInfo;
@@ -34,6 +36,7 @@ public class Phase1ProcurementMenusController {
 
     protected static final String ATTRIBUTE_PAGE_MODEL = "pageModel";
 
+    protected static final String PAGE_SINGLE_SITE_CONTINUITY = "chooseSingleSiteContinuity";
     protected static final String PAGE_PRICE_DECLARATION = "choosePriceDeclaration";
     protected static final String PAGE_SOLUTION_SET = "chooseSolutionSet";
 
@@ -241,6 +244,57 @@ public class Phase1ProcurementMenusController {
             + BuyingProcessController.PAGE_PROCUREMENT;
 	}
 
+	@GetMapping(value = {
+	              "/buyingprocess/choose-single-site-continuity",
+	              "/buyingprocess/{procurementId}/choose-single-site-continuity"
+	          })
+	public String chooseSingleSiteContinuity(@PathVariable Optional<Long> procurementId, Model model, HttpServletRequest request) throws ProcurementNotFoundException {
+	    SecurityInfo secInfo = SecurityInfo.getSecurityInfo(request);
+	    HttpSession session = request.getSession();
+
+	    Optional<SingleSiteContinuityEnum> set;
+	    if (procurementId.isPresent()) {
+	      Procurement procurement = procurementService.findById(procurementId.get());
+	      set = Optional.ofNullable(singleSiteContinuityBooleanToSingleSiteContinuity(procurement.getSingleSiteContinuity()));
+	    } else {
+	      Procurement.PrimitiveProcurement procurement = procurementService.getPrimitiveProcurement(session, secInfo);
+	      set = Optional.ofNullable(singleSiteContinuityBooleanToSingleSiteContinuity(procurement.getSingleSiteContinuity()));
+	    }
+
+	    var pageModel = new ChooseSingleSiteContinuityView();
+	    procurementId.ifPresent(pageModel::setId);
+	    set.ifPresent(element -> pageModel.setSelectedOption(element.getId()));
+
+	    model.addAttribute(ATTRIBUTE_PAGE_MODEL, pageModel);
+	    model.addAttribute("values", Arrays.asList(SingleSiteContinuityEnum.values()));
+
+	    return BuyingProcessController.PAGE_PATH + PAGE_SINGLE_SITE_CONTINUITY;
+	}
+
+	@PostMapping("/buyingprocess/choose-single-site-continuity")
+    public String chooseSingleSiteContinuity(ChooseSingleSiteContinuityView pageModel, HttpServletRequest request) throws ProcurementNotFoundException {
+  	    SecurityInfo secInfo = SecurityInfo.getSecurityInfo(request);
+  	    HttpSession session = request.getSession();
+
+  	    Optional<Long> procurementId = Optional.ofNullable(pageModel.getId());
+  	    SingleSiteContinuityEnum value = SingleSiteContinuityEnum.getById(pageModel.getSelectedOption());
+        if (procurementId.isPresent()) {
+          Procurement procurement = procurementService.findById(procurementId.get());
+          procurement.setSingleSiteContinuity(singleSiteContinuityToSingleSiteContinuityBoolean(value));
+          procurementService.save(procurement);
+        } else {
+          Procurement.PrimitiveProcurement procurement = procurementService.getPrimitiveProcurement(session, secInfo);
+          procurement.setSingleSiteContinuity(singleSiteContinuityToSingleSiteContinuityBoolean(value));
+          procurementService.setPrimitiveProcurement(session, procurement);
+        }
+
+        //TODO change to correct view when known
+        return REDIRECT_URL_PREFIX
+            + BuyingProcessController.URL_PATH
+            + "/"
+            + BuyingProcessController.PAGE_PROCUREMENT;
+	}
+
 	// Temporary page to kick off a procurement
 	@GetMapping(value = {"/buyingprocess/tmpBuyingStart"})
 	public String tmpBuyingStart(Model model, RedirectAttributes attr, HttpServletRequest request) {
@@ -276,6 +330,20 @@ public class Phase1ProcurementMenusController {
 
     private static Boolean solutionSetToFoundationBoolean(SolutionSetEnum solution) {
       return solution == null ? null : solution == SolutionSetEnum.FOUNDATION;
+    }
+
+    private static SingleSiteContinuityEnum singleSiteContinuityBooleanToSingleSiteContinuity(Boolean value) {
+      if (value == null) {
+        return null;
+      } else if (value) {
+        return SingleSiteContinuityEnum.SINGLE_SITE_CONTINUITY;
+      } else {
+        return SingleSiteContinuityEnum.NOT_SINGLE_SITE_CONTINUITY;
+      }
+    }
+
+    private static Boolean singleSiteContinuityToSingleSiteContinuityBoolean(SingleSiteContinuityEnum value) {
+      return value == null ? null : value == SingleSiteContinuityEnum.SINGLE_SITE_CONTINUITY;
     }
 
 }
---
+++ purchasing-frontend/src/main/java/uk/nhs/gpitf/purchasing/entities/SingleSiteContinuityEnum.java
@@ -0,0 +1,34 @@
+package uk.nhs.gpitf.purchasing.entities;
+
+import java.lang.invoke.MethodHandles;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lombok.Getter;
+
+@Getter
+public enum SingleSiteContinuityEnum {
+  NOT_SINGLE_SITE_CONTINUITY(1, "Not Single Site Continuity"),
+  SINGLE_SITE_CONTINUITY(2, "Single Site Continuity");
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private final long id;
+  private final String name;
+
+  private SingleSiteContinuityEnum(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public static SingleSiteContinuityEnum getById(Long id) {
+    return Stream.of(SingleSiteContinuityEnum.values())
+              .filter(e -> e.getId() == id)
+              .findFirst()
+              .orElseThrow(() -> {
+                LOGGER.warn("An attempt to get a SingleSiteContinuityEnum by id of \"{}\" occurred. But could not be found.", id);
+                return new NoSuchElementException("SingleSiteContinuityEnum " + id + " not found");
+              });
+  }
+}
